69b60a5c35fbe74870e7db131c7e6a5649d3803a
SiddharthKQP <siddharth.kulkarni@questionpro.com>
2022-03-01 23:18:52 +0530

    fixed customer service bugs

--- io/sidkulk/CustomerService.java
+++ io/sidkulk/CustomerService.java
@@ -3,23 +3,46 @@ package io.sidkulk;
 import java.util.Map;
 import java.util.TreeMap;
 import io.sidkulk.model.Customer;
+import java.util.Set;
+import java.util.Collections;
+import java.util.HashSet;
 
 public class CustomerService {
 	private Map<Integer, Customer> custMap = new TreeMap<>();
+	private Set<Integer> custIdSet = new HashSet<>();
+
 	public boolean addNewCustomer(Customer customer) {
-		if(customer == null) {
+		if (customer == null) {
 			return false;
 		} else {
-			custMap.put(customer.getId(), new Customer(customer.getFname(), customer.getLname(), customer.getAddress()));
+			int lastCustomerId = (int) Collections.max(custIdSet);
+			int newCustomerId = lastCustomerId + 1;
+			custMap.put(newCustomerId, new Customer(customer.getFname(), customer.getLname(), customer.getAddress()));
+			custIdSet.add(newCustomerId);
 			return true;
 		}
 	}
-	
+
 	public boolean deleteCustomerById(int custId) {
 		boolean isIdPresent = custMap.containsKey(custId);
-		if(isIdPresent) {
+		if (isIdPresent) {
 			Customer removedCustomer = custMap.remove(custId);
 			System.out.println("Customer removed from database: " + removedCustomer);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean updateCustomerById(int custId, Customer updatedValues) {
+		boolean isIdPresent = custMap.containsKey(custId);
+		if (isIdPresent) {
+			Customer updatedCustomer = custMap.get(custId);
+			updatedCustomer.setFname(updatedValues.getFname());
+			updatedCustomer.setLname(updatedValues.getLname());
+			updatedCustomer.setAddress(updatedValues.getAddress());
+
+			updatedCustomer = custMap.put(custId, updatedCustomer);
 			return true;
 		} else {
 			return false;
